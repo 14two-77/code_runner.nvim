5899a7fd5d67c621a5dc1463b838a6f24611df3f
Thanachart Sangmola <thanachart.farn@gmail.com>
2025-08-12 10:39:38 +0700

    Update run cmd to double quote only

--- lua/code_runner.lua
+++ lua/code_runner.lua
@@ -1,41 +1,47 @@
 local M = {}
 
 function M.run_cpp()
-	local file = vim.fn.expand("%:p")
-	local file_no_ext = vim.fn.expand("%:p:r")
-	local output
-	local compile_cmd
-	local run_cmd
-	local os_name = vim.loop.os_uname().sysname
+    local file = vim.fn.expand("%:p")
+    local file_no_ext = vim.fn.expand("%:p:r")
+    local output
+    local compile_cmd
+    local run_cmd
+    local os_name = vim.loop.os_uname().sysname
 
-	if os_name:match("Windows") then
-		output = file_no_ext .. ".exe"
-		compile_cmd = string.format('g++ \"%s\" -o \"%s\"', file, output)
-		run_cmd = string.format('"./%s"', output)
-	else
-		output = file_no_ext .. ".out"
-		compile_cmd = string.format('g++ \"%s\" -o \"%s\"', file, output)
-		run_cmd = string.format('"./%s"', output)
-	end
+    if os_name:match("Windows") then
+        output = file_no_ext .. ".exe"
+        compile_cmd = string.format('g++ \"%s\" -o \"%s\"', file, output)
+        run_cmd = string.format("./%s", output)
+    else
+        output = file_no_ext .. ".out"
+        compile_cmd = string.format('g++ \"%s\" -o \"%s\"', file, output)
+        run_cmd = string.format("./%s", output)
+    end
 
-	vim.cmd("w")
+    vim.cmd("w")
 
-  local Terminal = require("toggleterm.terminal").Terminal
-  if not M._cpp_term then
-    M._cpp_term = Terminal:new({ direction = "horizontal", close_on_exit = false })
-  end
+    local Terminal = require("toggleterm.terminal").Terminal
+    if not M._cpp_term then
+        M._cpp_term = Terminal:new({
+            direction = "horizontal",
+            close_on_exit = false
+        })
+    end
 
-  M._cpp_term:open()
+    M._cpp_term:open()
 
-  local term_cmd = string.format("%s && %s\n", compile_cmd, run_cmd)
-  M._cpp_term:send(term_cmd)
+    local term_cmd = string.format("%s && %s\n", compile_cmd, run_cmd)
+    M._cpp_term:send(term_cmd)
 end
 
 function M.setup()
-	local wk = require("which-key")
-	wk.add({
-		{ "<leader>r", M.run_cpp, desc = "Run current C++ file", mode = "n" },
-	})
+    local wk = require("which-key")
+    wk.add({{
+        "<leader>r",
+        M.run_cpp,
+        desc = "Run current C++ file",
+        mode = "n"
+    }})
 end
 
 return M
